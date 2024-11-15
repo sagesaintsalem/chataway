16cf3ece5dd2cef5cbf7076591f867054df24586
sagesaintsalem <ms.r.toussaint@gmail.com>
2024-11-15 10:09:44 +0000

    readMessage and sendMessage set up

--- Peer.cpp
+++ Peer.cpp
@@ -19,9 +19,35 @@ std::mutex cout_mutex;
 //}
 
 void Peer::readMessage() {
+    try {
 
+        boost::asio::streambuf buffer;
+        boost::asio::async_read_until(ssl_socket, buffer, "\n");
+        std::istream stream(&buffer);
+        std::string message;
+        std::getline(stream, message);
+
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        std::cout << message << std::endl; // Print received message
+
+    }
+    catch (const std::exception& error) {
+        std::cerr << "Read failed: " << error.what() << endl;
+
+    }
 }
 
 void Peer::sendMessage() {
-    
+    try {
+        string message;
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        message = name + ": " + message + "\n";  // Format message with username
+        boost::asio::async_write(ssl_socket, boost::asio::buffer(message));
+
+
+    }
+    catch (const std::exception& error) {
+        std::cerr << "Write failed: " << error.what() << endl;
+
+    }
 }
